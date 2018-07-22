ec036701c0a6003b07ba353eaaee19d9893c192f
Mdraa <memorycero@gmail.com>
2018-07-22 19:30:27 +0200

    Adding some measures in order to protect code from concurrency.

--- JSF_RPS_Project/main/java/com/cyclum/task/controller/GameController.java
+++ JSF_RPS_Project/main/java/com/cyclum/task/controller/GameController.java
@@ -1,9 +1,11 @@
 package com.cyclum.task.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.faces.context.FacesContext;
 
@@ -17,78 +19,92 @@ import com.cyclum.task.beans.GlobalScore;
  *
  */
 public class GameController {
-	
+
+	/** Logger */
+	private static final Logger LOGGER = Logger.getLogger(GameController.class.getName());
+
 	/** Map instance containing actual rounds for each session. */
-	private Map<String, List<GameRound>> actRoundsMapBySessionId;
-	
+	private volatile Map<String, List<GameRound>> actRoundsMapBySessionId;
+
 	/** Global score. */
 	private GlobalScore globalScore;
-	
+
 	/**
 	 * Contructor.
 	 */
 	public GameController() {
 		super();
-		this.actRoundsMapBySessionId = new HashMap<String, List<GameRound>>();
+		this.actRoundsMapBySessionId = new ConcurrentHashMap<String, List<GameRound>>();
 		this.globalScore = new GlobalScore();
 	}
-	
+
 	/**
 	 * Play round method.
 	 * 
 	 * @return
 	 */
-	public String playRound() {
-		//SessionId
-		String sessionId = getUserSessionId();
+	public synchronized String playRound() {
+		try {
+			// SessionId
+			String sessionId = getUserSessionId();
+
+			//We retrieve all rounds of current user.
+			List<GameRound> userRoundsList = actRoundsMapBySessionId.get(sessionId);
+			//New round
+			GameRound round = new GameRound();
+
+			//If the list is null we create a new one.
+			if (userRoundsList == null) {
+				userRoundsList = new ArrayList<GameRound>();
+			}
+
+			userRoundsList.add(round);
+			actRoundsMapBySessionId.put(sessionId, userRoundsList);
+
+			// Calculate global score.
+			getGlobalScore().updateGlobalScore(round);
 			
-		List<GameRound> userRoundsList = actRoundsMapBySessionId.get(sessionId);
-		GameRound round = new GameRound();
-		
-		if(userRoundsList == null) {
-			userRoundsList = new ArrayList<GameRound>();
+		} catch (Throwable th) {
+			LOGGER.log(Level.SEVERE, "Error playing a round !!!!");
 		}
-		
-		userRoundsList.add(round);
-		actRoundsMapBySessionId.put(sessionId, userRoundsList);
-		
-		//Calculate global score.
-		getGlobalScore().updateGlobalScore(round);
-			
+
 		return null;
 	}
 
 	/**
-	 * Method which restart actual game.
+	 * Restart method.
 	 */
 	public String restart() {
-		
-		String sessionId = getUserSessionId();
-		if (actRoundsMapBySessionId.containsKey(sessionId)) {
-			actRoundsMapBySessionId.get(sessionId).clear();
+		try {
+			String sessionId = getUserSessionId();
+			if (actRoundsMapBySessionId.containsKey(sessionId)) {
+				actRoundsMapBySessionId.get(sessionId).clear();
+			}
+		} catch (Throwable th) {
+			LOGGER.log(Level.SEVERE, "Error restarting the game !!!!");
 		}
-		
+
 		return null;
 	}
 
 	/**
-	 * @return the userRoundsList
+	 * @return actual user rounds list.
 	 */
 	public List<GameRound> getUserRoundsList() {
 		return actRoundsMapBySessionId.get(getUserSessionId());
 	}
-	
+
 	/**
 	 * @return the globalScore
 	 */
 	public GlobalScore getGlobalScore() {
 		return globalScore;
 	}
-	
+
 	/**
-	 * Method which returns user sessionId.
+	 * return sessionId.
 	 * 
-	 * @return sessionId
+	 * @return the sessionId
 	 */
 	public String getUserSessionId() {
 		return FacesContext.getCurrentInstance().getExternalContext().getSessionId(true);
